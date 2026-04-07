b9946ea5e2b3d5ec3a889a9a737a4a136efe5956
Zander Martineau <mrmartineau@users.noreply.github.com>
2026-04-07 23:13:44 +0300

    chore(ci): update package release config

--- release.config.mjs
+++ release.config.mjs
@@ -5,6 +5,14 @@ export default {
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    [
       '@semantic-release/github',
       {
         failComment: false,
